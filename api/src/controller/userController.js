10ced4e240d9977d3b6c7c08f8c2f100ecbe793b
victornpb <3372598+victornpb@users.noreply.github.com>
2021-04-10 20:53:20 -0300

    user controller

---
+++ api/src/controller/userController.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { User } = require('../model/User');
+
+const namespace = '/user';
+
+module.exports = (app) => {
+
+  const router = express.Router();
+  
+  router.post('/register', async (req, res) => {
+    console.log(req.body);
+    const { name, email, password } = req.body;
+
+    // Check if email already exists
+    if (await User.findOne({
+      email,
+    })) {
+      return res
+        .status(409)
+        .json({
+          code: 'EMAIL_ALREADY_EXIST',
+          message: 'User with the email already exists'
+        });
+    }
+
+    const user = {
+      name,
+      email,
+      password, // TODO: hash it
+    };
+
+    try {
+      const result = await User.create(user);
+
+      return res.json({
+        ...result.toJSON(),
+        password: undefined,
+      });
+    }
+    catch (err) {
+      res.status(500).send(err);
+    }
+
+  });
+
+  // register controller routes to app
+  app.use(namespace, router);
+};
--- api/src/server.js
+++ api/src/server.js
@@ -7,15 +7,21 @@ const port = process.env.API_PORT;
 
 const db = require('./database');
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const userController = require('./controller/userController');
+userController(app);
 
 const projectController = require('./controller/projectController');
 projectController(app);
 
+
 app.use('/explorer', explorer({format: 'html'}));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
