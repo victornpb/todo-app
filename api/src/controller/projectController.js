fa26ee09c6552c1dab30c3390fa6ebdb93ef8e3d
victornpb <3372598+victornpb@users.noreply.github.com>
2021-04-10 20:02:15 -0300

    create 'project' test

--- api/src/controller/projectController.js
+++ api/src/controller/projectController.js
@@ -6,12 +6,27 @@ const namespace = '/projects';
 module.exports = (app) => {
 
   const router = express.Router();
-
+  
+  // get
   router.get('/', async (req, res) => {
     const projects = await Project.find();
     return res.send({
       data: projects
     });
+  });
+
+  // create
+  router.post("/", async (req, res) => {
+    try {
+      const project = await Project.create({
+        name: 'Untitled',
+        userId: '1',
+      });
+  
+      res.send({ project });
+    } catch (error) {
+      return res.status(500);
+    }
   });
 
   // register controller routes to app
