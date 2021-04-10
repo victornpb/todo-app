abac076364ac234b56097eb3accb72c231ba3485
victornpb <3372598+victornpb@users.noreply.github.com>
2021-04-10 17:04:09 -0300

    implemeted db connection module

---
+++ api/src/database/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const mongodbUri = process.env.MONGODB_URI;
+if (!mongodbUri) throw new Error('Missing environment variable MONGODB_URI!');
+
+mongoose.connect(mongodbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (...args) => console.error('Error connecting to MongoDB!', ...args));
+db.once('open', () => {
+    console.log('Connected to MongoDB successfully!');
+    
+});
+
+module.exports = db;
