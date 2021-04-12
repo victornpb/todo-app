d05cf430b4870da5eae03aa3e173ad9b81432698
victornpb <3372598+victornpb@users.noreply.github.com>
2021-04-11 23:42:30 -0300

    Task component

--- front/src/components/Task.vue
+++ front/src/components/Task.vue
@@ -1,24 +1,101 @@
 <template>
-    <v-card>
+  <v-list-item :class="{ red: confirmDelete }">
+    <template v-if="!isDone">
+      <v-list-item-action>
+        <v-checkbox @click="check()" :input-value="data.status" :disabled="data.status" :loading="isLoading"></v-checkbox>
+      </v-list-item-action>
 
-    </v-card>
+      <v-list-item-content>
+        <v-list-item-title><v-text-field v-model="data.description" @change="updateTask()" hide-details /></v-list-item-title>
+      </v-list-item-content>
+
+      <v-list-item-action v-if="!confirmDelete">
+        <v-btn @click="confirmDelete = true" icon color="error"><v-icon small>remove_circle</v-icon></v-btn>
+      </v-list-item-action>
+      <v-list-item-action v-else>
+        <v-btn @click="confirmDelete = false" color="" outlined>Cancel</v-btn>
+        <v-btn @click="deleteTask()" color="" outlined>Remove</v-btn>
+      </v-list-item-action>
+    </template>
+    <template v-else>
+      <v-list-item-action>
+        <v-checkbox :input-value="data.status" :disabled="data.status" :loading="isLoading"></v-checkbox>
+      </v-list-item-action>
+
+      <v-list-item-content>
+        <v-list-item-title><v-text-field v-model="data.description" hide-details readonly /></v-list-item-title>
+      </v-list-item-content>
+
+      <v-list-item-action>
+        <v-btn :title="data.finished"><v-icon>info</v-icon></v-btn>
+      </v-list-item-action>
+    </template>
+  </v-list-item>
 </template>
 <script>
+import STATUS from '@/constants/taskStatus';
+
 export default {
-    components:{
-        
-    },
-    props: {
-        value: Object,
-    },
-    mounted(){
+  components: {},
+  props: {
+    value: Object,
+    projectId: String,
+  },
+  data() {
+    return {
+      STATUS,
 
-    },
-    computed:{
+      isLoading: false,
+      data: {},
+      error: null,
 
+      confirmDelete: false,
+    };
+  },
+  mounted() {
+    this.data = this.value;
+  },
+  computed: {
+    isDone() {
+      return this.data.status === STATUS.DONE;
     },
-    methods: {
-
-    }
-}
+  },
+  methods: {
+    check() {
+        if(this.data.status===STATUS.TODO){
+            this.data.status=STATUS.DONE;
+            this.updateTask();
+        }
+    },
+    async updateTask() {
+      this.error = null;
+      this.isLoading = true;
+      try {
+        await this.$put(`/tasks/${this.projectId}/${this.data._id}`, {
+          status: this.data.status,
+          description: this.data.description || 'Untitled Task',
+        });
+        this.data = task;
+      } catch (err) {
+        this.error = err && err.response && err.response.data.message;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    async deleteTask() {
+      this.error = null;
+      this.isLoading = true;
+      try {
+        const task = await this.$put(`/tasks/${this.projectId}/${this.data._id}`, {
+          status: STATUS.DONE,
+        });
+        this.data = task;
+      } catch (err) {
+        this.error = err && err.response && err.response.data.message;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+};
 </script>
