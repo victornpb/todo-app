3cb8de8a81c90eca7304d2c73ba5e5ea1e121cd7
victornpb <3372598+victornpb@users.noreply.github.com>
2021-04-12 00:36:59 -0300

    empty state project

--- front/src/components/Project.vue
+++ front/src/components/Project.vue
@@ -5,38 +5,44 @@
         <!-- {{ data.name }} -->
         <v-layout>
           <v-text-field v-model="data.name" @change="saveProject" :loading="isLoading" label="Project" hide-details />
-          <span class="headline" style="line-height: 48px;">{{ completed }}/{{ length }}</span>
+          <span class="headline" style="line-height: 48px">{{ completed }}/{{ length }}</span>
         </v-layout>
-        <v-progress-linear :value="completed/length*100" :disabled="!length" class="progress"></v-progress-linear>
+        <v-progress-linear :value="(completed / length) * 100" :disabled="!length" class="progress"></v-progress-linear>
       </v-expansion-panel-header>
-      <v-expansion-panel-content>
-     
-          <v-btn @click="deleteProject">Delete</v-btn>
-          <v-alert :value="error" type="error" dismissible>
-            <div>{{ error }}</div>
-          </v-alert>
+      <v-expansion-panel-content class="pa-0">
+        <v-btn @click="deleteProject">Delete</v-btn>
+        <v-alert :value="error" type="error" dismissible>
+          <div>{{ error }}</div>
+        </v-alert>
 
-          <v-list subheader>
-            <v-subheader>To do</v-subheader>
-            <template v-for="task in tasksTodo">
-              <Task :projectId="data._id" :value="task" :key="task._id" @deleted="taskDeleted(task)" />
-            </template>
-            <v-subheader>Done</v-subheader>
-            <template v-for="task in tasksDone">
-              <Task :projectId="data._id" :value="task" :key="task._id" />
-            </template>
-          </v-list>
+        <v-list v-if="!isEmpty" subheader>
+          <v-subheader>To do</v-subheader>
+          <template v-for="task in tasksTodo">
+            <Task :projectId="data._id" :value="task" :key="task._id" @deleted="taskDeleted(task)" />
+          </template>
+          <v-subheader>Done</v-subheader>
+          <template v-for="task in tasksDone">
+            <Task :projectId="data._id" :value="task" :key="task._id" />
+          </template>
+        </v-list>
 
-          <v-card>
-            <v-form @submit.prevent="addNewTask()">
-              <v-text-field v-model="newTaskDescription" :loading="isLoading" label="Add new task" outlined />
-              <v-btn :disabled="!newTaskDescription" type="submit">Add Task</v-btn>
-            </v-form>
-          </v-card>
+        <v-card v-if="isEmpty" elevation="0">
+          <v-card-text class="pa-4 text-center">
+            <v-icon size="96" style="opacity: 0.25">inbox</v-icon>
+            <p>This project is empty</p>
+            <p>Add a task below</p>
+            <v-icon>arrow_downward</v-icon>
+          </v-card-text>
+        </v-card>
 
+        <v-card>
+          <v-form @submit.prevent="addNewTask()">
+            <v-text-field v-model="newTaskDescription" :loading="isLoading" label="Add new task" outlined />
+            <v-btn :disabled="!newTaskDescription" type="submit">Add Task</v-btn>
+          </v-form>
+        </v-card>
 
-          <DeleteProjectDialog v-if="deleteProjectDialog" v-model="deleteProjectDialog" @changed="$emit('deleted')" />
-        
+        <DeleteProjectDialog v-if="deleteProjectDialog" v-model="deleteProjectDialog" @changed="$emit('deleted')" />
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
@@ -80,6 +86,10 @@ export default {
     tasksDone() {
       return this.data && this.data.tasks ? this.data.tasks.filter((task) => task.status) : [];
     },
+
+    isEmpty() {
+      return !(this.data && this.data.tasks && this.data.tasks.length);
+    },
   },
   methods: {
     async saveProject() {
@@ -120,14 +130,17 @@ export default {
     taskDeleted(task) {
       const index = this.data.tasks.indexOf(task);
       this.data.tasks.splice(index, 1);
-    }
+    },
   },
 };
 </script>
 <style scoped>
-.progress{
+.progress {
   position: absolute;
   top: 0px;
   left: 0px;
+}
+.v-expansion-panel-content__wrap{
+  padding:0;
 }
 </style>
