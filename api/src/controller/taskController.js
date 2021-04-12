02f04f914b2084e96a14bec93d2ad864da1f17e4
victornpb <3372598+victornpb@users.noreply.github.com>
2021-04-11 23:41:33 -0300

    delete task endpoint

--- api/src/controller/taskController.js
+++ api/src/controller/taskController.js
@@ -10,26 +10,6 @@ module.exports = (app) => {
   const router = express.Router();
   router.use(authMiddleware);
 
-  // get All
-  router.get('/', async (req, res) => {
-    const userId = req.userId;
-
-    const tasks = await Task.find().where({ userId: userId });
-    return res.send({
-      data: tasks,
-    });
-  });
-
-  // get All
-  router.get('/', async (req, res) => {
-    const userId = req.userId;
-
-    const tasks = await Task.find().where({ userId: userId });
-    return res.send({
-      data: tasks,
-    });
-  });
-
   // create
   router.post(
     '/:projectId',
@@ -129,7 +109,7 @@ module.exports = (app) => {
         task.finished = new Date();
         task.status = status;
       }
-      
+
       task.description = description || task.description;
 
       await project.save();
@@ -141,66 +121,44 @@ module.exports = (app) => {
   );
 
   // delete
-  router.delete('/:taskId', async (req, res) => {
+  router.delete('/:projectId/:taskId', async (req, res) => {
     const userId = req.userId;
-    const { taskId } = req.params;
+    const { projectId, taskId } = req.params;
 
-    const result = await Task.deleteOne({
-      userId: userId,
-      _id: taskId,
+    const project = await Project.findOne({
+      _id: projectId,
+      userId,
     });
 
-    if (result.deletedCount === 1) {
-      res.status(204).end();
-    } else {
-      res.status(404).json({
-        code: 'NOT_FOUND',
-        message: 'Task does not exist',
+    if (!project) {
+      return res.status(404).json({
+        code: 'PROJECT_NOT_FOUND',
+        message: 'You do not own a project with this id',
       });
     }
+
+    const task = project.tasks.find((task) => task.id === taskId);
+    if (!task) {
+      return res.status(404).json({
+        code: 'TASK_NOT_FOUND',
+        message: 'This project does not contain a task with this id',
+      });
+    }
+
+    if (task.status === STATUS.DONE) {
+      return res.status(403).json({
+        code: 'DELETE_NOT_ALLOWED',
+        message: 'Deleting a completed task is not allowed',
+      });
+    }
+
+    const index = project.tasks.indexOf(task);
+    project.tasks.splice(index, 1);
+
+    await project.save();
+
+    res.status(201).send();
   });
-
-  // update
-  router.put(
-    '/:taskId',
-    newSchemaValidator({
-      description: {
-        type: String,
-        required: false,
-      },
-      status: {
-        type: String,
-        enum: [STATUS.TODO, STATUS.DONE],
-        required: false,
-      },
-    }),
-    async (req, res) => {
-      const userId = req.userId;
-      const { taskId } = req.params;
-      const { description, status } = req.body;
-
-      const patch = {
-        name: name,
-      };
-
-      const result = await Task.updateOne(
-        {
-          userId: userId,
-          _id: taskId,
-        },
-        patch,
-      );
-
-      if (result.n === 1) {
-        res.status(204).end();
-      } else {
-        res.status(404).json({
-          code: 'NOT_FOUND',
-          message: 'Task does not exist',
-        });
-      }
-    },
-  );
 
   // register controller routes to app
   app.use(namespace, router);
